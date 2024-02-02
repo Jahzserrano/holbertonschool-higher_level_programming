626ccf7a8c152d34544253e694c3429b017bfb86
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-02-02 09:57:12 -0400

    added 2 task

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-search_replace.py
+# Jahaziel A. Serrano
+
+def search_replace(my_list, search, replace):
+    """Replace all element by a new list"""
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
