308224092f199eebe6f12468101eb2c103e9421c
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-02-02 10:00:59 -0400

    added task 3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+# jahaziel Adans Serrano
+
+
+def common_elements(set_1, set_2):
+    """Return a set of elements in two sets"""
+    return (set_1 & set_2)
