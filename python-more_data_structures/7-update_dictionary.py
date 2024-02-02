ca595a1639a27f522f81fc1eb2ff05c3f43ebda8
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-02-02 10:10:20 -0400

    added task 7

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 7-update_dictionary.py
+# Jahaziel Adans Serrano
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key value pairs"""
+    a_dictionary[key] = value
+    return (a_dictionary)
