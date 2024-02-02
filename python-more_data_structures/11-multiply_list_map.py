8d5edd8deb14ad7ed597a41fd4aa7bfb69f4947f
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-02-02 10:32:58 -0400

    added task 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# 11-multiply_list_map.py
-# Jahaziel Adans Serrano
-
 def multiply_list_map(my_list=[], number=0):
-    """Multiply a list by a selected  number"""
     return (list(map(lambda x: x*number, my_list)))
