b63ee00db0236872479e693cc691cf30ebdc0b09
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-02-17 16:17:24 -0400

    addde to Json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -25,3 +25,16 @@ class Base:
         else:
             Base.__nnb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+    
+    
