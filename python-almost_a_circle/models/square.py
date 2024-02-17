723befd006c59b0f175d44fe4565d9ed352f35be
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-02-17 14:16:47 -0400

    added square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Jahaziel Adans Serrano
+"""Define a square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A Square
+    
+    Args:
+        size (int): The size fo the Square
+        x (int): The coordinate x
+        y (int): The coordinate y
+        id (int): The id of the object
+    """
+    
+    def __init__(self, size, x=0, y=0, id=None):
+        super.__init__(size, size, x, y, id)
+    
