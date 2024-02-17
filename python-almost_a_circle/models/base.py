c84ca87718f8f61cdbd2573e50359e101207df27
Jahaziel Serrano <serranojahaziel2@gmail.com>
2024-02-17 10:31:30 -0400

    added structure and private variables

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Jahaziel Adans Serrano
+""" Defines a base model class"""
+
+
+class Base:
+    """Base Model
+
+        Base for the other classes
+        
+        Attributes:
+            __nb_objects (int): The number of instantiated Bases
+    """
+    
+    __nb_objects = 0
+    
